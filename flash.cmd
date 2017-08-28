6e17347a9da86d4fe1857d6b362ee26a8a721190
Maarten Pennings <30388931+maarten-pennings@users.noreply.github.com>
2017-08-28 22:47:57 +0200

    Made local (tool, firm) the default, added tee

--- flash.cmd
+++ flash.cmd
@@ -8,45 +8,57 @@ REM The !xx! is executed for each occurrence, %xx% is executed per block.
 REM A block is either everything between ( and ), or otherwise a line.
 
 
+REM This script uses the following trick to do the Unix trick cccc=`CCCC`
+REM   FOR /F "tokens=* USEBACKQ" %%F IN (`CCCC`) DO SET cccc=%%F
+
+
+CALL :mk_vbs
+
+
 REM === Logging =============================================================
 
 
 SET LOGFILE=%cd%\flash.log
-ECHO.Creating new log file > %LOGFILE%
+copy /y NUL  %LOGFILE% > NUL
 FOR /F "tokens=* USEBACKQ" %%F IN (`DATE /T`) DO SET log_date=%%F
 FOR /F "tokens=* USEBACKQ" %%F IN (`TIME /T`) DO SET log_time=%%F
 SET log_dir=%cd%
-CALL :log "  Started: %log_date% %log_time%" 0
-CALL :log "  Directory: %log_dir%" 0
-CALL :log "" 0
+CALL :log "flash.cmd by Maarten Pennings
+CALL :log "  %log_date%; %log_time%; %log_dir%"
+CALL :log ""
 
 
 REM === Find the flash tool =================================================
 
+
+SET tool_base=%cd%\
+SET tool_full=!tool_base!esptool.exe
+IF EXIST !tool_full! (
+  CALL :log "Flash tool found - with this script"
+  CALL :log "  !tool_full!"
+  CALL :log ""
+  GOTO :tool_done
+)
 
 SET tool_base=%appdata%\..\Local\Arduino15\packages\esp8266\tools\esptool\
 IF EXIST !tool_base! (
   FOR /F "tokens=* USEBACKQ" %%F IN (`dir /b !tool_base!`) DO SET tool_sub=%%F\
   SET tool_full=!tool_base!!tool_sub!esptool.exe
   IF EXIST !tool_full! (
-    CALL :log "Flash tool found - existing one"
+    CALL :log "Flash tool found - with Arduino IDE"
     CALL :log "  !tool_full!"
     CALL :log ""
     GOTO :tool_done
   ) ELSE (
-    CALL :log "WARNING This PC has Arduino and ESP8266 installed; failed to find:"
-    CALL :log "  !tool_full!"
+    CALL :log "Flash tool not found"
+    CALL :log "  This PC has Arduino with ESP8266 installed, but not !tool_full!"
+    SET fail_msg=Download ESPTOOL.EXE from https://github.com/igrr/esptool-ck/releases and copy next to this script
+    GOTO :abort
   )
-)
-
-SET tool_base=%cd%\
-SET tool_full=!tool_base!esptool.exe
-IF EXIST !tool_full! (
-  CALL :log "Flash tool found"
-  CALL :log "  !tool_full! "
-  CALL :log ""
 ) ELSE (
-  SET fail_msg=Failed to find ESPTOOL.EXE in script directory: !tool_full!
+  CALL :log "Flash tool not found"
+  CALL :log "  Failed to find ESPTOOL.EXE in: !cd!"
+  SET fail_msg=Download ESPTOOL.EXE from https://github.com/igrr/esptool-ck/releases and copy next to this script
   GOTO :abort
 )
 
@@ -56,27 +68,29 @@ IF EXIST !tool_full! (
 REM === Find the firmware ===================================================
 
 
-SET firm_base=%appdata%\..\Local\Temp\
-FOR /F "tokens=* USEBACKQ" %%F IN (`dir /b /o /d !firm_base!arduino_build_* 2^> nul`) DO SET firm_sub=%%F\
-SET firm_base=!firm_base!!firm_sub!
-FOR /F "tokens=* USEBACKQ" %%F IN (`dir /b /o /d !firm_base!*.bin 2^> nul`) DO SET firm_file=%%F
+SET firm_base=%cd%\
+FOR /F "tokens=* USEBACKQ" %%F IN (`dir /b !firm_base!*.bin 2^> NUL`) DO SET firm_file=%%F
 SET firm_full=!firm_base!!firm_file!
 IF EXIST !firm_full! (
-  CALL :log "Firmware found - in temporary Arduino build dir"
+  CALL :log "Firmware found - with this script"
   CALL :log "  !firm_full!"
   CALL :log ""
   GOTO :firm_done
 )
 
-SET firm_base=%cd%\
-FOR /F "tokens=* USEBACKQ" %%F IN (`dir /b !firm_base!*.bin`) DO SET firm_file=%%F
+SET firm_base=%appdata%\..\Local\Temp\
+FOR /F "tokens=* USEBACKQ" %%F IN (`dir /b /o /d !firm_base!arduino_build_* 2^> NUL`) DO SET firm_sub=%%F\
+SET firm_base=!firm_base!!firm_sub!
+FOR /F "tokens=* USEBACKQ" %%F IN (`dir /b /o /d !firm_base!*.bin 2^> NUL`) DO SET firm_file=%%F
 SET firm_full=!firm_base!!firm_file!
 IF EXIST !firm_full! (
-  CALL :log "Firmware found"
+  CALL :log "Firmware found -  - with Arduino IDE"
   CALL :log "  !firm_full!"
   CALL :log ""
 ) ELSE (
-  SET fail_msg=Failed to find firmware: !firm_base!*.bin
+  CALL :log "Firmware not found"
+  CALL :log "  Failed to find firmware: !cd!\*.bin"
+  SET fail_msg=Download some *.bin file and copy next to this script
   GOTO :abort
 )
 
@@ -117,7 +131,7 @@ IF %com_count% EQU 1 (
   REM Next blocks should be in else, but the () break the %% in !com_all:%com_sel%=!
 )
 
-CALL :input "Enter COM port for ESP8266/NodeMCU\n\nAvailable ports: %com_all%" "Enter COM port" "%com_last%"
+CALL :inputbox "Enter COM port for ESP8266/NodeMCU\n\nAvailable ports: %com_all%" "Enter COM port" "%com_last%"
 SET com_sel=!input!
 IF "Q%com_sel%" == "Q" (
   SET fail_msg=Aborted by user
@@ -140,23 +154,24 @@ REM === Execute flashing ===================================================
 SET cmd=%tool_full%  -cd nodemcu  -cb 512000  -cp %com_sel%  -cf %firm_full%
 CALL :log "Command"
 CALL :log "  %cmd%"
-CALL :message "Confirm flashing of '%firm_file%' to '%com_sel%'\nNote: flashing will take up to 30 seconds."  1  "Confirm"
+CALL :msgbox "Confirm flashing of '%firm_file%' to '%com_sel%'.\n\nNote: flashing will take up to 30 seconds."  1  "Confirm"
 SET cmd_ack=%input%
 IF %cmd_ack% NEQ 1 (
   SET fail_msg=Aborted by user
   GOTO :abort
 )
 
-ECHO.============================================================================================ >> %LOGFILE%
-%cmd% >> %LOGFILE%
-ECHO.============================================================================================ >> %LOGFILE%
+CALL :log ""
+CALL :log "========================================================================================="
+%cmd%  |  cscript //nologo %tee% %LOGFILE%
+CALL :log "========================================================================================="
 IF %ERRORLEVEL% GTR 0 (
   SET fail_msg=Flashing failed
   GOTO :abort
 ) 
 FOR /f "tokens=* delims=" %%g in ('findstr "100" %LOGFILE%') DO SET cmd_result=%%g
 IF "Q!cmd_result:100=!" == "Q!cmd_result!" (
-  SET fail_msg=Image was not flashed, or not flashed completely
+  SET fail_msg=Image was not flashed - try running script again
   GOTO :abort
 )
 
@@ -167,8 +182,9 @@ REM === Terminate ===========================================================
 :success
 CALL :log ""
 CALL :log "Completed successfully"
-CALL :log "  See %LOGFILE% for details"
-CALL :message "Success; see %LOGFILE% for log."  0  "Information"
+CALL :log "  See %LOGFILE% for a copy of this output"
+CALL :msgbox "Success\n\nSee %LOGFILE% for log."  0  "Information"
+CALL :rm_vbs
 EXIT /B 0
 
 
@@ -176,38 +192,53 @@ REM === Subroutines =========================================================
 
 
 :abort
-CALL :log "Error: !fail_msg!"
 CALL :log ""
 CALL :log "Aborted"
-CALL :log "  See %LOGFILE% for log"
-CALL :message "%fail_msg%"  0  "Critical Error"
+CALL :log "  !fail_msg!"
+CALL :log "  See %LOGFILE% for a copy of this output"
+CALL :msgbox "%fail_msg%"  0  "Critical Error"
+CALL :rm_vbs
 EXIT /B 1
 
 
 :log
-REM Add entry to log file and console; call as   CALL :log "entry" ["0"]
-IF NOT "Q%~2" == "Q0" ECHO.%~1
+REM Add entry to log file and console; call as   CALL :log "entry"
+ECHO.%~1
 ECHO.%~1 >> %LOGFILE%
 EXIT /B
 
 
-:message
-REM Show a message with buttons; call as   CALL :message "message" "buttons" "title"   reply comes in   %input%
-ECHO WScript.Echo MsgBox(Replace(WScript.Arguments(0),^"\n^",vbNewline),WScript.Arguments(1),WScript.Arguments(2)) > "%temp%\flash.messagebox.vbs"
-REM ECHO WScript.Echo MsgBox(WScript.Arguments(0),WScript.Arguments(1),WScript.Arguments(2)) > "%temp%\flash.messagebox.vbs"
-FOR /f "tokens=* delims=" %%g in ('cscript //Nologo "%temp%\flash.messagebox.vbs" "%~1" "%~2" "flash.cmd - %~3"') DO SET input=%%g
-DEL "%temp%\flash.messagebox.vbs"
+:mk_vbs
+REM msgbox
+SET msgbox="%temp%\flash.msgbox.vbs"
+ECHO.WScript.Echo MsgBox(Replace(WScript.Arguments(0),^"\n^",vbNewline),WScript.Arguments(1),WScript.Arguments(2)) > %msgbox%
+REM inputbox
+SET inputbox="%temp%\flash.inputbox.vbs"
+ECHO.WScript.Echo "A"^&InputBox(Replace(WScript.Arguments(0),^"\n^",vbNewline),WScript.Arguments(1),WScript.Arguments(2)) > %inputbox%
+REM tee
+SET tee="%temp%\flash.tee.vbs"
+ECHO.Set file = CreateObject("Scripting.FileSystemObject").OpenTextFile(WScript.Arguments(i),8,True) > %tee%
+ECHO.Do Until Wscript.StdIn.AtEndOfStream: line=Wscript.StdIn.ReadLine: Wscript.StdOut.WriteLine(line): file.WriteLine(line): Loop >> %tee%
 EXIT /B
 
 
-:input 
-REM Prompt for input; call as   CALL :input "prompt" "title" "default"  reply comes in   %input% (empty on cancel)
+:rm_vbs
+DEL %msgbox%
+DEL %inputbox%
+DEL %tee%
+EXIT /B
+
+
+:msgbox
+REM Show a message with buttons; call as   CALL :msgbox "message" "buttons" "title"   reply comes in   %input%
+FOR /f "tokens=* delims=" %%g in ('cscript //Nologo %msgbox% "%~1" "%~2" "flash.cmd - %~3"') DO SET input=%%g
+EXIT /B
+
+
+:inputbox
+REM Prompt for input; call as   CALL :inputbox "prompt" "title" "default"  reply comes in   %input% (empty on cancel)
 SET input=
-ECHO WScript.Echo "A"^&InputBox(Replace(WScript.Arguments(0),^"\n^",vbNewline),WScript.Arguments(1),WScript.Arguments(2)) > "%temp%\flash.inputbox.vbs"
-FOR /f "tokens=* delims=" %%g in ('cscript //Nologo "%temp%\flash.inputbox.vbs" "%~1" "flash.cmd - %~2" "%~3"') DO SET input=%%g
+FOR /f "tokens=* delims=" %%g in ('cscript //Nologo %inputbox% "%~1" "flash.cmd - %~2" "%~3"') DO SET input=%%g
 SET input=%input:~1%
-DEL "%temp%\flash.inputbox.vbs"
 EXIT /B
 
-
-REM last confirm more text and new lines
